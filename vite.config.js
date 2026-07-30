4004bcb960537c0fb15905e01c3fa6b8cfceb86f
Keneil2 <190867803+Keneil-Samms@users.noreply.github.com>
2026-07-29 22:36:07 -0400

    chore: installed and configure tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
-
+import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
     plugins: [
         laravel({
@@ -16,5 +16,7 @@ export default defineConfig({
                 },
             },
         }),
+        
+        tailwindcss()
     ],
 });
