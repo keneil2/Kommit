f554e9f65af9eea7aab265c2ef9d1f454c839809
Keneil2 <190867803+Keneil-Samms@users.noreply.github.com>
2026-08-10 23:59:14 -0400

    chore: setup tailwind and updated gitignore

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,13 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
-import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.ts',
+            input: ['resources/js/app.ts','resources/css/app.css'],
             refresh: true,
         }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
+        vue(),
         
-        tailwindcss()
     ],
 });
