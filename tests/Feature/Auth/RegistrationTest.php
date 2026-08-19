15f44a568c3fd7300f41e7709b7d2d04a7a4f552
Keneil2 <190867803+Keneil-Samms@users.noreply.github.com>
2026-08-18 20:27:47 -0500

    test: added email verification tests

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -1,19 +1,70 @@
 <?php
 
-test('registration screen can be rendered', function () {
-    $response = $this->get('/register');
+use App\Mail\EmailVerification;
+use App\Mail\WelcomeMail;
+use Hoa\Event\Test\Unit\Event;
+use Illuminate\Support\Facades\Log;
+use Inertia\Testing\AssertableInertia as Assert;
 
+
+test("assert registration page can be rendered",function(){
+     $this->actingAsGuest();
+    $response = $this->get("/login");
     $response->assertStatus(200);
 });
 
-test('new users can register', function () {
-    $response = $this->post('/register', [
-        'name' => 'Test User',
-        'email' => 'test@example.com',
-        'password' => 'password',
-        'password_confirmation' => 'password',
+
+
+test('assert user can be registered', function () {
+    $this->actingAsGuest();
+    $response = $this->post('/register',[
+    "email"=>"test@gmail.com",
+    "password"=>"test@1234",
+    "password_confirmation"=>"test@1234"
+    ]);
+    $response->assertStatus(302);
+});
+
+
+test('assert email verification screen is rendered', function () {
+     
+    $this->actingAsGuest();
+    $response = $this->post('/register',[
+    "name"=>"tester1",
+    "email"=>"test@gmail.com",
+    "password"=>"test@1234",
+    "password_confirmation"=>"test@1234"
+    ]);
+    
+    $response->assertRedirect("email-verification?email=test%40gmail.com");
+});
+
+test('assert verification and Welcomemail are queued when email is sent', function () {
+    Mail::fake();
+    $this->actingAsGuest();
+    $response = $this->post('/register',[
+    "name"=>"tester1",
+    "email"=>"test@gmail.com",
+    "password"=>"test@1234",
+    "password_confirmation"=>"test@1234"
     ]);
 
-    $this->assertAuthenticated();
-    $response->assertRedirect(route('dashboard', absolute: false));
+    $response->assertStatus(302);
+
+    Mail::assertQueued(WelcomeMail::class);
+    Mail::assertQueued(EmailVerification::class);
+    
 });
+
+// test('assert email can be email', function () {
+//     Mail::fake();
+//     $this->actingAsGuest();
+//     $response = $this->post('/register',[
+//     "email"=>"test@gmail.com",
+//     "password"=>"test@1234",
+//     "password_confirmation"=>"test@1234"
+//     ]);
+    
+//     $response->assertRedirect("/email-verification");
+// });
+
