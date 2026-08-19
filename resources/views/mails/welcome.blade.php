c974d2532c73b669655193b024569d00d4464cb4
Keneil2 <190867803+Keneil-Samms@users.noreply.github.com>
2026-08-18 20:25:32 -0500

    dev:email verification email templates added

---
+++ resources/views/mails/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <meta charset='utf-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <title>Welcome email</title>
+    <link rel='stylesheet' href='main.css'>
+</head>
+<body>
+    Hi {{ $name }} welcome to Kommit nice to have you. dont forget to kommit that link. 
+</body>
+</html>
